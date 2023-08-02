d2721f46f1ea0e850df683cf01c7b86b8b3d35e9
Daniel Vazač <daniel.vazac@email.cz>
2023-08-03 01:43:07 +0200

    node

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
+        adapter: adapter()
     },
 	preprocess: vitePreprocess()
 };
