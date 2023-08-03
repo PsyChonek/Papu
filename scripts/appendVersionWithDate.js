ec879f1cf44c8360cb6a7f8013bebc4fce0fc8c1
Daniel Vazač <daniel.vazac@email.cz>
2023-08-03 14:25:19 +0200

    Version

--- scripts/appendVersionWithDate.js
+++ scripts/appendVersionWithDate.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 // get package.json version and append version with date
-// version format '0.1.6 - 20230717'
+// version format '0.1.6 - 2023/07/17 18:34'
 
 // Open file
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
@@ -8,8 +8,8 @@ const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 // Get version from package.json
 const version = packageJson.version;
 
-// Get date
-const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+// Get date '0.1.6 - 2023/07/17 18:34'
+const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').split(':').slice(0, -1).join(':').replace(/-/g, '/');
 
 // Remove date from version
 const versionWithoutDate = version.split(' - ')[0];
@@ -26,4 +26,5 @@ fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
 // Write version to .env
 fs.writeFileSync('./.env', `REACT_APP_VERSION=${versionWithDate}`);
 
+// Log version
 console.log(`versionWithDate: ${versionWithDate}`);
