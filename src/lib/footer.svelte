a45a52140d8f1be2c23ecd5447374f7a7b21f978
Daniel Vazač <daniel.vazac@email.cz>
2023-09-01 22:33:24 +0200

    Version to .env

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,12 +1,12 @@
 <!-- Footer with version, github link and author -->
 <script>
-    import {version} from '$app/environment' 
+	import {PUBLIC_APP_VERSION} from '$env/static/public'
 </script>
 
 <footer class="m-auto">
 	<div class="flex flex-row items-center justify-center gap-10">
 		<a href="https://github.com/PsyChonek/Papu" class="hover:scale-150 text-center font-bold text-orange-500">Github</a>
 		<a href="authors" class="hover:scale-150 text-center font-bold text-blue-600">Authors</a> 
-		<h1 class="text-center">{version}UTC</h1>
+		<h1 class="text-center">{PUBLIC_APP_VERSION}UTC</h1>
 	</div>
 </footer>
