ccbb627c55b9898f356deb22a768878e00e00d21
Daniel Vazač <daniel.vazac@email.cz>
2023-10-01 18:10:11 +0200

    only first

--- scripts/replace_env.sh
+++ scripts/replace_env.sh
@@ -2,13 +2,14 @@
 
 while IFS= read -r line || [[ -n "$line" ]]; do
     if [[ $line == *=* ]] && [[ ! $line == \#* ]]; then
-        key="${line%=*}"
+        key="${line%%=*}" # Get everything before the first '='
         secret_key="${key^^}" # Convert to uppercase
 
         # Check if the variable is set in the GitHub Actions environment
         if secret_value=$(printenv "$secret_key"); then
             echo "Processing: $secret_key" # Debugging line
-            sed -i "s|$key=.*|$key=\"$secret_value\"|" .env
+            # Use a more robust sed command to replace the value
+            sed -i "s|^\($key=\).*|\1\"$secret_value\"|" .env
         else
             echo "Skipping: $secret_key (not set in GitHub Actions environment)"
         fi
