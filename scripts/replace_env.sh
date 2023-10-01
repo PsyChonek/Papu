b22e814b99ac255042e4be0a265a70a01559e6ff
Daniel Vazač <daniel.vazac@email.cz>
2023-10-01 17:42:53 +0200

    push

--- scripts/replace_env.sh
+++ scripts/replace_env.sh
@@ -4,12 +4,11 @@ while IFS= read -r line || [[ -n "$line" ]]; do
     if [[ $line == *=* ]] && [[ ! $line == \#* ]]; then
         key="${line%=*}"
         secret_key="${key^^}" # Convert to uppercase
-        echo "Processing: $secret_key" # Debugging line
 
         # Check if the variable is set in the GitHub Actions environment
-        if printenv "$secret_key" > /dev/null; then
-            secret_value=$(printenv "$secret_key")
-            sed -i "s|$key=.*|$key=$secret_value|" .env
+        if secret_value=$(printenv "$secret_key"); then
+            echo "Processing: $secret_key" # Debugging line
+            sed -i "s|$key=.*|$key=\"$secret_value\"|" .env
         else
             echo "Skipping: $secret_key (not set in GitHub Actions environment)"
         fi
