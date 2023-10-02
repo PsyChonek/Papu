125ac80d471fa29f70640c2924a3516ef4ec58f5
Daniel Vazač <daniel.vazac@email.cz>
2023-10-03 00:17:37 +0200

    nicer logger

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -1,64 +1,27 @@
 import winston, { format } from 'winston';
 
-/**
- * Example from github
- */
+// Define a format for console output with colorization
+const consoleFormat = format.combine(
+	format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+	format.colorize(),
+	format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+// Define a format for file output without colorization
+const fileFormat = format.combine(
+	format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+	format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
 export const logger = winston.createLogger({
-	format: format.combine(
-		format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-		format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
-	),
 	transports: [
 		//
 		// - Write to all logs with level `info` and below to `combined.log`
 		// - Write all logs error (and below) to `error.log`.
 		//
-		new winston.transports.File({ filename: 'logs/debug.log', level: 'debug' }),
-		new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-		new winston.transports.File({ filename: 'logs/combined.log' })
+		new winston.transports.File({ filename: 'logs/debug.log', level: 'debug', format: fileFormat }),
+		new winston.transports.File({ filename: 'logs/error.log', level: 'error', format: fileFormat }),
+		new winston.transports.File({ filename: 'logs/combined.log', format: fileFormat }),
+		new winston.transports.Console({ format: consoleFormat }) // Use the console format for console transport
 	]
 });
-
-logger.add(
-	new winston.transports.Console({
-		format: format.combine(
-			format.colorize(),
-			format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-			format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
-		),
-		level: 'debug'
-	})
-);
-
-logger.add(
-	new winston.transports.Console({
-		format: format.combine(
-			format.colorize(),
-			format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-			format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
-		),
-		level: 'info'
-	})
-);
-
-logger.add(
-	new winston.transports.Console({
-		format: format.combine(
-			format.colorize(),
-			format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-			format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
-		),
-		level: 'warn'
-	})
-);
-
-logger.add(
-	new winston.transports.Console({
-		format: format.combine(
-			format.colorize(),
-			format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-			format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
-		),
-		level: 'error'
-	})
-);
