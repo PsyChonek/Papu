d0f398773b66c7e4bbd294c3e2cb13ac80617768
Daniel Vazač <daniel.vazac@email.cz>
2023-10-09 23:17:59 +0200

    Animation

--- src/lib/components/home/sideBar.svelte
+++ src/lib/components/home/sideBar.svelte
@@ -3,6 +3,7 @@
 	import { generateKey } from '$lib/keys';
 	import { orders, orderKeyStore } from '$lib/stores';
 	import type { Order } from '$lib/types/order';
+	import { slide } from 'svelte/transition';
 	var showSideBar = true;
 
 	function dateFormat(date: string): string {
@@ -40,50 +41,41 @@
 	}
 </script>
 
-{#if showSideBar}
-	<div class="bg-red-50 h-screen w-80 fixed left-0 top-0">
-		<div class="flex flex-row justify-around m-5 items-baseline">
-			<h1 class="font-bold text-xl">Orders</h1>
-			<button on:click={() => (showSideBar = !showSideBar)} class="border-2 border-orange-500 rounded-xl p-1">⬅️</button>
-		</div>
-
-		<!-- Place reverse order -->
-		{#each $orders.sort(orderOrdersByDate) as order}
-			<div class="flex flex-row justify-between rounded-xl p-2 px-4 items-center gap-2">
-				<button
-					on:click={() => {
-						setActive(order.key);
-					}}
-					class="flex flex-row justify-between bg-blue-100 border-blue-400 rounded-xl p-2 items-baseline grow"
-				>
-					<div>
-						<p class="font-bold text-xl">{dateFormat(order.date)}</p>
-						<p class="font-bold text-xl">{timeFormat(order.date)}</p>
-					</div>
-					<div>
-						<p class="font-bold text-xl">{order.key}</p>
-						<p class="font-bold text-xl">{arraySum(order.participants.map((participant) => participant.total))}</p>
-					</div>
-				</button>
-				<!-- Only allow removing not active orders -->
-				{#if order.key !== $orderKeyStore}
-					<button on:click={() => removeOrder(order.key)} class="border-2 border-orange-500 rounded-xl p-1">🗑️</button>
-				{/if}
-				{#if order.key === $orderKeyStore}
-					<button class="border-2 border-orange-500 rounded-xl p-1 items-center">✅</button>
-				{/if}
-			</div>
-		{/each}
-
-		<div class="flex flex-row justify-around m-5 items-baseline">
-			<button on:click={() => addOrder()} class="border-2 border-orange-500 rounded-xl p-1">New order</button>
-		</div>
+<div class="bg-red-50 h-screen w-80 fixed left-0 top-0" transition:slide={{ duration: 300, axis: 'x' }}>
+	<div class="flex flex-row justify-around m-5 items-baseline">
+		<h1 class="font-bold text-xl">Orders</h1>
+		<button on:click={() => (showSideBar = !showSideBar)} class="border-2 border-orange-500 rounded-xl p-1">⬅️</button>
 	</div>
-{/if}
-{#if !showSideBar}
-	<div class="h-screen w-30 fixed left-0 top-0">
-		<div class="flex flex-row justify-around m-5 items-baseline">
-			<button on:click={() => (showSideBar = !showSideBar)} class="border-2 border-orange-500 rounded-xl p-1">➡️</button>
+
+	<!-- Place reverse order -->
+	{#each $orders.sort(orderOrdersByDate) as order}
+		<div class="flex flex-row justify-between rounded-xl p-2 px-4 items-center gap-2">
+			<button
+				on:click={() => {
+					setActive(order.key);
+				}}
+				class="flex flex-row justify-between bg-blue-100 border-blue-400 rounded-xl p-2 items-baseline grow"
+			>
+				<div>
+					<p class="font-bold text-xl">{dateFormat(order.date)}</p>
+					<p class="font-bold text-xl">{timeFormat(order.date)}</p>
+				</div>
+				<div>
+					<p class="font-bold text-xl">{order.key}</p>
+					<p class="font-bold text-xl">{arraySum(order.participants.map((participant) => participant.total))}</p>
+				</div>
+			</button>
+			<!-- Only allow removing not active orders -->
+			{#if order.key !== $orderKeyStore}
+				<button on:click={() => removeOrder(order.key)} class="border-2 border-orange-500 rounded-xl p-1">🗑️</button>
+			{/if}
+			{#if order.key === $orderKeyStore}
+				<button class="border-2 border-orange-500 rounded-xl p-1 items-center">✅</button>
+			{/if}
 		</div>
+	{/each}
+
+	<div class="flex flex-row justify-around m-5 items-baseline">
+		<button on:click={() => addOrder()} class="border-2 border-orange-500 rounded-xl p-1">New order</button>
 	</div>
-{/if}
+</div>
